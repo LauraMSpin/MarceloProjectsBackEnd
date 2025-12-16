8180b8894410747473277e7a09711e0cf4ed67eb
LauraMSpin <lauramendespin@gmail.com>
2025-12-16 20:58:59 -0300

    feat: Add Dependencias table and relationships in the database schema

--- Migrations/20251213161316_AddDependencias.Designer.cs
+++ Migrations/20251213161316_AddDependencias.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using cronograma_atividades_backend.Data;
@@ -11,9 +12,11 @@ using cronograma_atividades_backend.Data;
 namespace cronograma_atividades_backend.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251213161316_AddDependencias")]
+    partial class AddDependencias
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
